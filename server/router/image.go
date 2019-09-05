ae7559cea9107c83adad46d996d27f65025759e7
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-05 23:18:09 +0900

    画像を投稿できるようにした

--- server/router/image.go
+++ server/router/image.go
@@ -1,7 +1,10 @@
 package router
 
 import (
+	"fmt"
+	"io"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/oribe1115/portfolio/server/model"
@@ -14,6 +17,14 @@ func PostNewSubImageHandler(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusBadRequest, "bad request")
 	}
 
+	filePath, err := uploadSubImage(c)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "faild to save sub image")
+	}
+
+	subImage.URL = filePath
+
 	newSubImage, err := model.NewSubImage(&subImage)
 	if err != nil {
 		c.Logger().Error(err)
@@ -21,4 +32,32 @@ func PostNewSubImageHandler(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, newSubImage)
+}
+
+func uploadSubImage(c echo.Context) (string, error) {
+	file, err := c.FormFile("subImage")
+	if err != nil {
+		fmt.Println("faild to get")
+		return "", err
+	}
+
+	src, err := file.Open()
+	if err != nil {
+		fmt.Println("faild to file open")
+		return "", err
+	}
+	defer src.Close()
+
+	dst, err := os.Create("/portfolio/images/subImages/" + file.Filename)
+	if err != nil {
+		fmt.Println("faild to create")
+		return "", err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, src); err != nil {
+		return "", err
+	}
+
+	return "/portfolio/images/subImages" + file.Filename, nil
 }
