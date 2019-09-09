77a7a89bbe54ebb28539d13bccc88b067f84dc10
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-09 11:24:33 +0900

    mainImage投稿用のdbまわりの関数を実装

--- server/model/image.go
+++ server/model/image.go
@@ -27,6 +27,36 @@ func GetSubImageList() ([]*SubImage, error) {
 	return subImageList, nil
 }
 
+func NewMainImage(mainImage *MainImage) (*MainImage, error) {
+	if err := db.Create(mainImage).Error; err != nil {
+		return nil, err
+	}
+	return mainImage, nil
+}
+
+func GetMainImage(mainImageID uuid.UUID) (*MainImage, error) {
+	mainImage := &MainImage{}
+	if err := db.Where("id = ?", mainImageID).First(mainImage).Error; err != nil {
+		return nil, err
+	}
+	return mainImage, nil
+}
+
+func GetMainImageByContentID(contentID uuid.UUID) (*MainImage, error) {
+	mainImage := &MainImage{}
+	if err := db.Where("content_id = ?", contentID).First(mainImage).Error; err != nil {
+		return nil, err
+	}
+	return mainImage, nil
+}
+
+func DeleteMainImage(mainImage *MainImage) error {
+	if err := db.Delete(mainImage).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
 func IsExistSubImageID(subImageID uuid.UUID) bool {
 	count := 0
 	if err := db.Table("sub_images").Where("id = ?", subImageID).Count(&count).Error; err != nil {
@@ -40,4 +70,12 @@ func DeleteSubImage(subImage *SubImage) error {
 		return err
 	}
 	return nil
+}
+
+func IsExistMainImage(contentID uuid.UUID) bool {
+	count := 0
+	if err := db.Table("main_images").Where("content_id = ?", contentID).Count(&count).Error; err != nil {
+		return false
+	}
+	return count > 0
 }
