1b3953ccfd24bd3bb2f48da7c82c352993d4b26e
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-09 11:40:31 +0900

    一応mainImageを投稿できるようになったが、更新ができていない

--- server/main.go
+++ server/main.go
@@ -53,6 +53,7 @@ func main() {
 
 	api.POST("/content/:contentID/tag/:tagID", router.PostNewTaggedContentHandler)
 	api.POST("/content/:contentID/subImage", router.PostNewSubImageHandler)
+	api.POST("/content/:contentID/mainImage", router.PostMainImageHandler)
 
 	api.GET("/tag", router.GetTagListHandler)
 	api.POST("/tag", router.PostNewTagHandler)
--- server/router/image.go
+++ server/router/image.go
@@ -79,6 +79,57 @@ func DeleteSubImageHandler(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
+func PostMainImageHandler(c echo.Context) error {
+	pathParam := c.Param("contentID")
+	contentID, err := uuid.Parse(pathParam)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid uuid")
+	}
+
+	if !model.IsExistContentID(contentID) {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusBadRequest, "invalid contentID")
+	}
+
+	if model.IsExistMainImage(contentID) {
+		oldMainImage, err := model.GetMainImageByContentID(contentID)
+		if err != nil {
+			c.Logger().Error(err)
+			return echo.NewHTTPError(http.StatusInternalServerError, "faild to get old main image")
+		}
+		err = model.DeleteMainImage(oldMainImage)
+		if err != nil {
+			c.Logger().Error(err)
+			return echo.NewHTTPError(http.StatusInternalServerError, "faild to delete on db")
+		}
+		err = deleteImage(oldMainImage.Name)
+		if err != nil {
+			c.Logger().Error(err)
+			return echo.NewHTTPError(http.StatusInternalServerError, "faild to delete file")
+		}
+	}
+
+	fileName, err := uploadImage(c)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "faild to save sub image")
+	}
+
+	mainImage := model.MainImage{}
+	mainImage.Name = fileName
+	mainImage.URL = c.Scheme() + "://" + c.Request().Host + "/images/" + fileName
+	mainImage.ContentID = contentID
+
+	newMainImage, err := model.NewMainImage(&mainImage)
+	if err != nil {
+		c.Logger().Error(err)
+		return echo.NewHTTPError(http.StatusInternalServerError, "faild to save")
+	}
+
+	return c.JSON(http.StatusCreated, newMainImage)
+}
+
 func uploadImage(c echo.Context) (string, error) {
 	file, err := c.FormFile("image")
 	if err != nil {
