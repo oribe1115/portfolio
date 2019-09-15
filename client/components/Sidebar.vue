b3843717f75fc37f3ea0edc07fe037f022fd5eb9
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-15 21:51:58 +0900

    わからず

---
+++ client/components/Sidebar.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+    h1 Sidebar
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/pages/_mainCategory/index.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    h1 here is main category
+    .sidebar
+        Sidebar
+</template>
+
+<script>
+
+import Sidebar from "@/components/Sidebar.vue"
+
+export default {
+    name: "MainCategory",
+    components: {
+        Sidebar
+    }
+}
+</script>
