299df7560c321e98b8d7b0376672364631b43a71
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-15 18:54:41 +0900

    pugを導入

---
+++ client/components/Sidebar.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+    
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import {SubCategory, MainCategory} from '../types/Portfolio'
+export default class Sidebar extends Vue{
+}
+</script>
