9acff6f5ad071230405a4bc7a1a80a60c270b541
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-18 17:53:31 +0900

    sidebarをつくった

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    .sidebar
+        h1 sidebar
+        p {{ this.test }}
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "Sidebar",
+  data() {
+    return {
+      test: []
+    };
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/api/category")
+      .then(res => (this.test = res.data));
+  }
+};
+</script>
---
+++ client/src/views/Main.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+    .main
+        h1 main
+        Sidebar
+</template>
+<script>
+import Sidebar from "@/components/Sidebar.vue";
+
+export default {
+  name: "Main",
+  components: {
+    Sidebar
+  }
+};
+</script>
