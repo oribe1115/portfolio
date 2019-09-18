346b3661f799e612cb14cf01b10cc4401ad02141
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-18 20:53:40 +0900

    stateにidを渡せるようにした

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -2,10 +2,11 @@
     .sidebar
         h1 sidebar
         p {{ fromStore }}
-        .main-categories
-            .main-item(v-for="mainCategory in mainCategories" :key = "mainCategory.id")
+        .main-category(v-for="mainCategory in mainCategories" :key = "mainCategory.id")
+            .main-item(@click="clickMainCategory(mainCategory.id)")
                 p {{ mainCategory.name }}
-                .sub-categoies(v-for="subCategory in mainCategory.sub_categories" :key "subCategory.id")
+            .sub-category(v-for="subCategory in mainCategory.sub_categories" :key "subCategory.id")
+                .sub-item(@click="clickSubCategory(subCategory.id)")
                     p {{ subCategory.name }}
 </template>
 <script>
@@ -20,10 +21,18 @@ export default {
       fromStore: ""
     };
   },
+  methods: {
+    clickMainCategory(id) {
+      store.setToMainCategory(id);
+    },
+    clickSubCategory(id) {
+      store.setToSubCategory(id);
+    }
+  },
   mounted() {
     axios.get("/api/category").then(res => (this.mainCategories = res.data));
     store.setToMainCategory("id");
-    this.fromStore = store.state.target;
+    this.fromStore = store.state;
   }
 };
 </script>
