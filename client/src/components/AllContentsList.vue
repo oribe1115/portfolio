16a0caf6b7727453517371ef5fbc243877886b60
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-19 10:26:05 +0900

    contentnslistの取得

--- client/src/components/AllContentsList.vue
+++ client/src/components/AllContentsList.vue
@@ -1,9 +1,20 @@
 <template lang="pug">
     .all-contents-list
         h1 All Contents List
+        p {{ contents }}
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  name: "AllContentsList"
+  name: "AllContentsList",
+  data() {
+    return {
+      contents: []
+    };
+  },
+  mounted() {
+    axios.get("/api/content").then(res => (this.contents = res.data));
+  }
 };
 </script>
