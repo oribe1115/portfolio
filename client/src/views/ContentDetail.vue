541f34164242bf95cfd6e3dca901e762ef2a7f2d
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-09-19 10:41:37 +0900

    ContentDetailで情報取得

--- client/src/views/ContentDetail.vue
+++ client/src/views/ContentDetail.vue
@@ -2,14 +2,23 @@
     .content-detail
         h1 Content Detail
         p contentID: {{ contentID }}
+        p content: {{ content.title }}
 </template>
 <script>
+import axios from "axios";
+
 export default {
   name: "ContentDetail",
   data() {
     return {
-      contentID: this.$route.params.contentID
+      contentID: this.$route.params.contentID,
+      content: null
     };
+  },
+  mounted() {
+    axios
+      .get("/api/content/" + this.contentID)
+      .then(res => (this.content = res.data));
   }
 };
 </script>
