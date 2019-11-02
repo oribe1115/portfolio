f42c0afa55831a351c1378b385cec447b17babeb
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-11-02 18:00:08 +0900

    contentへの遷移

--- client/src/components/ContentsList.vue
+++ client/src/components/ContentsList.vue
@@ -2,6 +2,10 @@
     .contents-list
         h1 ContentsList
         p {{ contents }}
+        .list(v-for="content in contents" :key="content.id")
+          router-link(:to="{ name: 'content', params: { contentID: content.id } }")
+            .content
+              p {{ content.title }}
 </template>
 
 <script>
