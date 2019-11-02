fc2172f5aa3864a1aa4d88cde49849edfe0f6849
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-11-02 12:12:15 +0900

    Sidebar作成

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+    h1 Sidebar
+</template>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <Sidebar />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Sidebar from "@/components/Sidebar.vue";
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    // HelloWorld,
+    Sidebar
   }
 };
 </script>
