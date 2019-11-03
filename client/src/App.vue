80727410b793169f6405b82155688593fbbd9b78
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-11-03 09:52:28 +0900

    aboutページで情報を表示するようにした

--- client/src/App.vue
+++ client/src/App.vue
@@ -37,6 +37,6 @@ export default {
 }
 
 .view {
-  width: 800px;
+  width: 900px;
 }
 </style>
--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -1,10 +1,44 @@
 <template lang="pug">
   .about
     h1 about
+    .content
+      markdown-it-vue(:content="about.content")
 </template>
 
+<script>
+import axios from "axios";
+import MarkdownItVue from "markdown-it-vue";
+import "markdown-it-vue/dist/markdown-it-vue.css";
+
+export default {
+  name: "About",
+  components: {
+    MarkdownItVue
+  },
+  data() {
+    return {
+      about: null
+    };
+  },
+  created() {
+    this.fetchData();
+  },
+  watch: {
+    $route: "fetchData"
+  },
+  methods: {
+    fetchData: function() {
+      axios.get("/api/generalData/about").then(res => {
+        this.about = res.data;
+      });
+    }
+  }
+};
+</script>
+
 <style lang="scss">
-.about {
-  width: 80%;
+.content {
+  text-align: left;
+  padding: 30px;
 }
 </style>
