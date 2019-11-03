c94e73a68c8b5c7ee99d40eaa4f9151fe4460e67
oribe(MacBookPro) <ayaka.k1n05h17a@gmail.com>
2019-11-03 18:08:46 +0900

    パス周り

--- server/main.go
+++ server/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gorilla/sessions"
@@ -43,6 +48,52 @@ func main() {
 	e := echo.New()
 	e.Debug = true
 	e.Use(middleware.Logger())
+
+	// middleware.StaticWithConfigからほぼコピペ・一部改変
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) (err error) {
+			p := c.Request().URL.Path
+			if strings.HasSuffix(c.Path(), "*") { // When serving from a group, e.g. `/static*`.
+				p = c.Param("*")
+			}
+			p, err = url.PathUnescape(p)
+			if err != nil {
+				return
+			}
+			name := filepath.Join("./static", path.Clean("/"+p)) // "/"+ for security
+
+			fi, err := os.Stat(name)
+			if err != nil {
+				if os.IsNotExist(err) {
+					if err = next(c); err != nil {
+						if he, ok := err.(*echo.HTTPError); ok {
+							if he.Code == http.StatusNotFound {
+								return c.File("./static/index.html")
+							}
+						}
+						return
+					}
+				}
+				return
+			}
+
+			if fi.IsDir() {
+				// トップページ('/')にアクセスしてきた時
+				index := filepath.Join(name, "index.html")
+				fi, err = os.Stat(index)
+
+				if err != nil {
+					if os.IsNotExist(err) {
+						return next(c)
+					}
+					return
+				}
+				return c.File(index)
+			}
+
+			return c.File(name)
+		}
+	})
 
 	e.Static("/", "/portfolio/server/static/")
 	e.File("/defaultImage", "/portfolio/server/static/NoImage.png")
